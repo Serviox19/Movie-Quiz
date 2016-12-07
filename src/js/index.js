7fbd9629da3a21613423230f84ae91ada5a24992
Servio Mora <servio.mora19@gmail.com>
2016-12-07 18:35:08 -0500

    displaying each movie successfully

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,23 @@ $(document).ready(function () {
   apiUrl += "/3/discover/movie";
   apiUrl += "?api_key=" + apiKey;
   apiUrl += "&with_people=287";
-  apiUrl += "&sort_by=";
+  apiUrl += "&sort_by=popularity.desc";
 
   $.getJSON(apiUrl, function (data) {
     for (var i = 0; i < data.results.length; i++) {
-      console.log(data.results[i]);
+      var movies = data.results[i];
+      console.log(movies);
+
+      $('#content').append('<div class="col-md-4">\
+                              <div class="card"\
+                                <img class="responsive-img card-img-top" src="https://image.tmdb.org/t/p/w500' + movies.backdrop_path + '" alt="Card image cap">\
+                                <div class="card-block">\
+                                  <h4 class="card-title">'+ movies.title +'</h4>\
+                                  <p class="card-text">'+ movies.overview +'</p>\
+                                  <a href="#" class="btn btn-primary">Quiz</a>\
+                                </div>\
+                              <div>\
+                            <div>');
     }
   });
 
