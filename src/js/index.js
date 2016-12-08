8094d04d9a2ad175ec9a13ee364da36b22b6bd9d
Servio Mora <servio.mora19@gmail.com>
2016-12-07 19:32:20 -0500

    added modals for more information on each movie, front end done

--- src/js/index.js
+++ src/js/index.js
@@ -17,10 +17,26 @@ $(document).ready(function () {
                                 <img src="https://image.tmdb.org/t/p/w500' + movies.backdrop_path +'" alt="">\
                                 <div class="caption">\
                                   <h3>'+ movies.title +'</h3>\
-                                  <p><a href="#" class="btn btn-primary" role="button">Info</a> <a href="#" class="btn btn-default" role="button">Quiz</a></p>\
+                                  <p><a class="btn btn-primary" role="button" data-toggle="modal" data-target="#'+ movies.id +'">Info</a> <a href="#" class="btn btn-default" role="button">Quiz</a></p>\
                                 </div>\
                               </div>\
-                            </div>');
+                            </div>\
+      <div class="modal fade" id="'+ movies.id +'" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+        <div class="modal-dialog" role="document">\
+          <div class="modal-content">\
+            <div class="modal-header">\
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+                <span aria-hidden="true">&times;</span>\
+              </button>\
+              <h4 class="modal-title" id="myModalLabel">'+ movies.title +'</h4>\
+            </div>\
+            <div class="modal-body">'+ movies.overview +'</div>\
+            <div class="modal-footer">\
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
+            </div>\
+          </div>\
+        </div>\
+      </div>');
     }
   });
 
